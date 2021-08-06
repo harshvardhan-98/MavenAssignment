813758de29ad327048ce5805022c894b56b800b5
Harsh <harsh.vardhan@knoldus.com>
2021-08-06 16:13:04 +0530

    Initial commit

---
+++ calApi/src/main/java/org/Calculator/calapi/Calculator.java
@@ -0,0 +1,23 @@
+package org.Calculator.calapi;
+
+public class Calculator {
+    public int add(int a, int b)
+    {
+        return a+b;
+    }
+
+    public int subtract(int a, int b)
+    {
+        return a-b;
+    }
+
+    public int multiply(int a, int b)
+    {
+        return a*b;
+    }
+
+    public double divide(double a, double b)
+    {
+        return a/b;
+    }
+}
